624bf32de5065c986576f976ca85127172817592
soheekim <65839795+soheekimdev@users.noreply.github.com>
2024-10-07 17:45:07 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,36 @@
+function findMaxCountItem(obj, count) {    
+    return Number(Object.keys(obj).find(key => obj[key] === count));
+}
+
+function solution(array) {
+    let answer;
+    
+    // array의 각 item과 count 값을 itemCountMap에 담기
+    const itemCountMap = {};
+    array.forEach((item => {
+        if(!itemCountMap[item]) {
+            itemCountMap[item] = 1;
+        } else {
+            itemCountMap[item]++;
+        }
+    }));
+    
+    // maxCount 구하기
+    let maxCount = 1;
+    const counts = Object.values(itemCountMap);
+    counts.forEach(count => {
+        if(count > maxCount) {
+            maxCount = count;
+        }
+    })
+    
+    // maxCount를 value로 갖는 keys 구하기
+    const maxCountItems = Object.entries(itemCountMap).filter(([ , count]) => count === maxCount);
+    if(maxCountItems.length > 1) {
+        answer = -1;
+    } else {
+        answer = findMaxCountItem(itemCountMap, maxCount);   
+    }
+    
+    return answer;
+}
